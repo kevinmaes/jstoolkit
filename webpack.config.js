8f50e1207fe97a6ee59fb472b1c7efdefa325416
Tom Ragle <tragle@gmail.com>
2016-03-26 09:01:12 -0400

    Adding webpack and a simple React app

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry:  __dirname + "/src/app.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+
+  module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel',
+          query: {
+            presets: ['es2015','react']
+          }
+        }
+      ]
+    }
+};

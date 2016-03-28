b872c912650e83f21b2dff426bf10d75dcffd84d
Kevin Maes <kevin@tek212.com>
2016-03-28 14:38:36 -0400

    fix lint error

--- src/examples/App.jsx
+++ src/examples/App.jsx
@@ -14,7 +14,7 @@ class Counter extends Component {
 
   tick() {
     this.setState({
-      counter: this.state.counter + this.props.increment
+      counter: this.state.counter + this.props.increment,
     });
   }
 

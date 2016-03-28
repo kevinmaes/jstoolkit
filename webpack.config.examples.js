e737941e62b0afdf2e2e81db5e41df40e408b8c4
Kevin Maes <kevin@tek212.com>
2016-03-28 13:33:49 -0400

    make examples config just like normal config

--- webpack.config.examples.js
+++ webpack.config.examples.js
@@ -26,8 +26,9 @@ module.exports = {
       {
         test: [/\.js$/, /\.jsx$/],
         loaders: ['babel', 'eslint-loader'],
+        include: path.join(__dirname, 'src'),
         exclude: /node_modules/,
-        loader: 'babel'
+        // loader: 'babel'
       }
     ]
   }

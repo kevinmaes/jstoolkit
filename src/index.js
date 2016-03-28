bd4e54a7a2f99f5ba7f8b3ba9e0aa455676dd9e1
Kevin Maes <kevin@tek212.com>
2016-03-28 19:06:57 -0400

    revise index.js welcome message so something shows on screen.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { render } from 'react-dom';
 
 function main() {
   const app = document.createElement('div');
+  app.innerHTML = "Welcome to the Nitro Client Scaffolding"
   document.body.appendChild(app);
   // render(<App />, app);
 }

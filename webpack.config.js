503d8faeb5c0fb35f4516e8673ca8bc7158f96a9
Kevin Maes <kevin@tek212.com>
2016-03-28 11:26:43 -0400

    add source map to config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
+  // or devtool: 'eval' to debug issues with compiled output:
+  devtool: 'cheap-module-eval-source-map',
   entry:  __dirname + "/src/app.js",
   output: {
     path: path.join(__dirname, 'dist'),

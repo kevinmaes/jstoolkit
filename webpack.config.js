6e58a6e51ea4606908b7d800e46df43d9f12cfbd
Kevin Maes <kevin@tek212.com>
2016-03-28 11:39:16 -0400

    add to entry config and add resolve extensions

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,17 @@ var webpack = require('webpack')
 module.exports = {
   // or devtool: 'eval' to debug issues with compiled output:
   devtool: 'cheap-module-eval-source-map',
-  entry:  __dirname + "/src/app.js",
+  entry: [
+    // necessary for hot reloading with IE:
+    //'eventsource-polyfill',
+    // listen to code updates emitted by hot middleware:
+    'webpack-hot-middleware/client',
+    // your code:
+    './src/index'
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: "bundle.js",

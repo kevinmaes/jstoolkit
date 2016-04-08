304e3bf9177f1caf6fff4b25843e90aea3a9db7f
Kevin Maes <kevin@tek212.com>
2016-04-07 23:16:49 -0400

    use es2015 in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-function main() {
+const main = () => {
   const app = document.createElement('div');
   app.innerHTML = 'Welcome to the Nitro Client Scaffolding';
   document.body.appendChild(app);
   render(<div>This is the Nitro Client Scaffolding</div>, app);
-}
+};
 
 main();

201b48c1e41ed49385e04ac52a4a8c950c64a3e3
Kevin Maes <kevin@tek212.com>
2016-04-07 22:24:01 -0400

    tweak custom eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
    },
   // See ESLint Rules: http://eslint.org/docs/rules/
   "rules": {
-    "semi": ["off", "always"],
-    "comma-dangle": ["error", "always-multiline"]
+    "semi": ["error", "always"],
+    "comma-dangle": ["off", "always-multiline"]
   }
 };

8ed505bd2977d667a4d6d9b7143271a42f450b2b
Kevin Maes <kevin@tek212.com>
2016-04-20 20:53:17 -0400

    augment spec_helper with new testing tools

--- src/tests/spec_helper.js
+++ src/tests/spec_helper.js
@@ -1,4 +1,15 @@
-import _ from 'lodash';
-import test from 'tape-catch';
+import tape from 'tape';
+import * as tapeCatch from 'tape-catch';
+import _ from 'lodash/fp';
+import deepFreeze from 'deep-freeze';
+import sinon from 'sinon';
+import createComponent from 'react-unit';
+import { createRenderer } from 'react-addons-test-utils';
+import jsxEquals from 'tape-jsx-equals';
 
-export { test, _ };
+// Extend tape with addiional assertions from tape-catch and jsxEquals.
+import addAssertions from 'extend-tape';
+const newAssertions = Object.assign({}, tapeCatch, { jsxEquals });
+const test = addAssertions(tape, newAssertions);
+
+export { test, _, deepFreeze, sinon, createComponent, createRenderer };

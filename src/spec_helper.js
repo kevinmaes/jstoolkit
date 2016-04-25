1f433789a94f9c4c0d4fe87f15064a9e3eb3f98f
Tom Ragle <tragle@gmail.com>
2016-04-25 17:11:26 -0400

    Pulling in log-flush style build and tests

--- src/spec_helper.js
+++ src/spec_helper.js
@@ -1,15 +1,11 @@
-import tape from 'tape';
-import * as tapeCatch from 'tape-catch';
+import tape from 'tape-catch';
 import _ from 'lodash/fp';
 import deepFreeze from 'deep-freeze';
-import sinon from 'sinon';
 import createComponent from 'react-unit';
 import { createRenderer } from 'react-addons-test-utils';
 import jsxEquals from 'tape-jsx-equals';
-
 // Extend tape with addiional assertions from tape-catch and jsxEquals.
 import addAssertions from 'extend-tape';
-const newAssertions = Object.assign({}, tapeCatch, { jsxEquals });
-const test = addAssertions(tape, newAssertions);
+const test = addAssertions(tape, { jsxEquals });
 
-export { test, _, deepFreeze, sinon, createComponent, createRenderer };
+export { test, _, deepFreeze, createComponent, createRenderer };

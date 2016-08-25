505b6b3b8b036f50cdb4ea28e5d0d9f8c3fa8c42
Kevin Maes <kevin@tek212.com>
2016-08-25 16:51:57 -0400

    reinstate airbnb and fix some lint errors

--- src/__test__/index.spec.js
+++ src/__test__/index.spec.js
@@ -19,7 +19,7 @@ test.skip('src basic arithmetic', (t) => {
   t.plan(4);
 
   t.equal(2 + 3, 5);
-  t.equal(7 * 8 + 9, 65);
+  t.equal((7 * 8) + 9, 65);
   t.equal(1 + 2, 3);
   t.equal(3 + 4, 7);
 });
@@ -28,7 +28,7 @@ test.skip('src basic arithmetic 2', (t) => {
   t.plan(4);
 
   t.equal(2 + 3, 5);
-  t.equal(7 * 8 + 9, 65);
+  t.equal((7 * 8) + 9, 65);
   t.equal(1 + 2, 3);
   t.equal(3 + 4, 7);
 });

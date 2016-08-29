ff9c92b5406882ba3e46fe890a4019894f21dada
Kevin Maes <kevin@tek212.com>
2016-08-29 18:44:20 -0400

    add tests. break out functions

--- src/utils/__test__/keyMirror.spec.js
+++ src/utils/__test__/keyMirror.spec.js
@@ -1,16 +1,37 @@
 import { test } from '../../spec_helper';
-import keyMirror from '../keyMirror';
+import keyMirror, { getKeys } from '../keyMirror';
 
-test('[keyMirror]', (t) => {
+test('[keyMirror] getKeys', (t) => {
   const array = ['a', 'b', 'c'];
 
-  const result = keyMirror(array);
+  const result = getKeys(array);
 
-  t.deepEquals(result, {
-    a: 'a',
-    b: 'b',
-    c: 'c'
-  }, 'create a key mirror for an array with keys as element values');
+  t.deepEquals(result, array, 'keys should be a similar array');
+
+  t.end();
+});
+
+test('[keyMirror] getKeys', (t) => {
+  const array = ['a', 'b', 'c'];
+
+  const result = getKeys(array);
+
+  t.deepEquals(result, array, 'keys should be a similar array');
+
+  t.end();
+});
+
+test('[keyMirror]', (t) => {
+  const obj = {
+    a: null,
+    b: null,
+    c: null
+  };
+
+  const result = getKeys(obj);
+
+  t.deepEquals(result, ['a', 'b', 'c'],
+    'keys should be an array of the object keys');
 
   t.end();
 });

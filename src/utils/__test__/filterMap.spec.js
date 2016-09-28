e6e24d9fe87e6dba4ff38ee4caa42a44ad7e9a95
Kevin Maes <kevin@tek212.com>
2016-09-28 12:11:21 -0400

    add 'until' param. add tests

--- src/utils/__test__/filterMap.spec.js
+++ src/utils/__test__/filterMap.spec.js
@@ -63,24 +63,117 @@ test('[filterMap] omitting the filter and map args', (t) => {
   t.end();
 });
 
+test('[filterMap] passing in a filter function, "isEven"', (t) => {
+  const array = [1, 2, 3, 4, 5]
+  const isEven = arg => !(arg % 2)
 
-//
-// /* IMPLEMENTATION */
-//
-// // Map iterator to get the value of an object's id.
-// const getId = prop.bind(null, 'id')
-//
-// // Map iterator to get an object's val property.
-// const getVal = prop.bind(null, 'val')
-//
-// // Example: Use getter truthy/falsy result as filter.
-// // Filter an array using getId (sometimes returns undefined) as the filter iterator and then map the filtered results of an array of objects.
-// // Only take the first 3 filtered results.
-// result = filterMap(getId, getVal, 3, [
-//   { id: 1, val: '1' },
-//   { id: 2, val: '2' },
-//   { id: 3, val: '3' },
-//   { id: undefined, val: '4' },
-//   { id: 5, val: '5' }
-// ])
-// console.log('result', result) // ['1', '2', '3'] instead of // ['1', '2', '3', '5']
+  const result = filterMap(isEven, undefined, array);
+
+  t.deepEquals(result, [2, 4], 'return value should be only even values');
+
+  t.end();
+});
+
+test('[filterMap] passing in a filter function, "isString"', (t) => {
+  const array = [1, 2, 3, 4, 5]
+  const isString = arg => typeof arg === 'string'
+
+  const result = filterMap(isString, undefined, array);
+
+  t.deepEquals(result, [],
+    'return value should be an empty array (no strings)');
+
+  t.end();
+});
+
+test('[filterMap] passing in a map function (no filter function)', (t) => {
+  const array = [
+    { id: 1, val: '1' },
+    { id: 2, val: '2' },
+    { id: 3, val: '3' },
+    { id: 4, val: '4' },
+    { id: 5, val: '5' }
+  ]
+  const getVal = arg => arg.val
+
+  const result = filterMap(undefined, getVal, array);
+
+  t.deepEquals(result, ['1', '2', '3', '4', '5'],
+    'return value should be an array of object values');
+
+  t.end();
+});
+
+test('[filterMap] passing in filter and map functions', (t) => {
+  const array = [
+    { id: 1, val: '1' },
+    { id: 2, val: '2' },
+    { id: 3, val: '3' },
+    { id: 4, val: '4' },
+    { id: 5, val: '5' }
+  ]
+  const isEven = arg => !(arg.id % 2)
+  const getVal = arg => arg.val
+
+  const result = filterMap(isEven, getVal, array);
+
+  t.deepEquals(result, ['2', '4'],
+    'return value should be an array of values of the filtered object array');
+
+  t.end();
+});
+
+test('[filterMap] passing in a composed filter function and map function', (t) => {
+  const array = [
+    { id: 1, val: '1' },
+    { id: 2, val: '2' },
+    { id: 3, val: '3' },
+    { id: 4, val: '4' },
+    { id: 5, val: '5' }
+  ]
+  const id = arg => arg.id
+  const isEven = arg => !(arg % 2)
+  const idIsEven = _.compose(isEven, id)
+  const getVal = arg => arg.val
+
+  const result = filterMap(idIsEven, getVal, array);
+
+  t.deepEquals(result, ['2', '4'],
+    'return value should be an array of values of the composite filtered object array');
+
+  t.end();
+});
+
+test('[filterMap] passing in comosed filter and map functions', (t) => {
+  const array = [
+    { id: 1, val: '1' },
+    { id: 2, val: '2' },
+    { id: 3, val: '3' },
+    { id: 4, val: '4' },
+    { id: 5, val: '5' }
+  ]
+  const id = arg => arg.id
+  const isEven = arg => !(arg % 2)
+  const idIsEven = _.compose(isEven, id)
+  const getVal = arg => arg.val
+  const getValAsNumber = _.compose(Number, getVal)
+
+  const result = filterMap(idIsEven, getValAsNumber, array);
+
+  t.deepEquals(result, [2, 4],
+    'return value should be an array of values of the composite filtered object array');
+
+  t.end();
+});
+
+test('[filterMap] passing in an optional "until" value', (t) => {
+  const array = [1, 2, 3, 4, 5]
+  const isOdd = arg => !!(arg % 2)
+
+  const result = filterMap(isOdd, undefined, array, 2);
+
+  t.deepEquals(result, [1, 3],
+    'return value should be an array containing only the first 2 odd values');
+
+  t.end();
+});
--- src/utils/filterMap.js
+++ src/utils/filterMap.js
@@ -7,9 +7,9 @@ export const property = (prop = '', obj) =>
 
 // A reducer implementation that simultanously filters and maps over an array,\
 // only looping over the array once.
-export default (filter = identity, map = identity, array = []) =>
+export default (filter = identity, map = identity, array = [], until) =>
   array.reduce((acc, next) => {
-    if (acc.length < array.length && filter(next))
+    if (acc.length < (until || array.length) && filter(next))
     	acc.push(map(next))
     return acc
   }, [])

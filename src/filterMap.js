0e94f7d19126398f7e6532c73187159107c5baa6
Kevin Maes <kevin@tek212.com>
2016-09-29 11:49:02 -0400

    add docs for filterMap

--- src/filterMap.js
+++ src/filterMap.js
@@ -1,8 +1,18 @@
 import { identity, result } from './utils/'
 
-// filterMap() utility.
-// A reducer implementation that simultanously filters and then maps over an array,
-// only looping over the array once.
+/**
+ * A reducer implementation that simultaneously filters and then maps over
+ * an array, only looping over the array once.
+ *
+ * @param {function} filter - A filter iterator function.
+ * @param {function:string} map - A map iterator function or a string
+ * representing a property name or method name on each array element.
+ * @param {*[]} array - The array to filter and/or map over.
+ * @param {number} [until] - An optional number indicating the max number of
+ * desired array results.
+ *
+ * @return {string[]} An array of key values (strings).
+ */
 export default (filter = identity, map = identity, array = [], until) =>
   array.reduce((acc, next) => {
     if (acc.length < (until || array.length) && filter(next)) {

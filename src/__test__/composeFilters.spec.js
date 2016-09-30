cf766e99906dd9818214550df600148caa1f28d0
Kevin Maes <kevin@tek212.com>
2016-09-29 22:29:56 -0400

    remove semicolons

--- src/__test__/composeFilters.spec.js
+++ src/__test__/composeFilters.spec.js
@@ -5,27 +5,27 @@ test('[composeFilters] single filter returning true' , t => {
   const f = td.func('one')
   td.when(f()).thenReturn(true)
 
-  const subject = composeFilters(f);
+  const subject = composeFilters(f)
   const result = subject()
 
   t.ok(result,
-    'composed filter should return true if its one filter returns true');
+    'composed filter should return true if its one filter returns true')
 
-  t.end();
-});
+  t.end()
+})
 
 test('[composeFilters] single filter returning false' , t => {
   const f = td.func('one')
   td.when(f()).thenReturn(false)
 
-  const subject = composeFilters(f);
+  const subject = composeFilters(f)
   const result = subject()
 
   t.notOk(result,
-    'composed filter should return false if its one filter returns false');
+    'composed filter should return false if its one filter returns false')
 
-  t.end();
-});
+  t.end()
+})
 
 test('[composeFilters] should return true 2/2 filters return true', t => {
   const f1 = td.func('one')
@@ -33,14 +33,14 @@ test('[composeFilters] should return true 2/2 filters return true', t => {
   td.when(f1()).thenReturn(true)
   td.when(f2()).thenReturn(true)
 
-  const subject = composeFilters(f1, f2);
+  const subject = composeFilters(f1, f2)
   const result = subject()
 
   t.ok(result,
-    'composed filter should return true if 2/2 filters return true');
+    'composed filter should return true if 2/2 filters return true')
 
-  t.end();
-});
+  t.end()
+})
 
 test('[composeFilters] should return false if 2/2 filters return false', t => {
   const f1 = td.func('one')
@@ -48,14 +48,14 @@ test('[composeFilters] should return false if 2/2 filters return false', t => {
   td.when(f1()).thenReturn(false)
   td.when(f2()).thenReturn(false)
 
-  const subject = composeFilters(f1, f2);
+  const subject = composeFilters(f1, f2)
   const result = subject()
 
   t.notOk(result,
-    'composed filter should return false if 2/2 filters return false');
+    'composed filter should return false if 2/2 filters return false')
 
-  t.end();
-});
+  t.end()
+})
 
 test('[composeFilters] should return false if the first of 2 filters return false', t => {
   const f1 = td.func('one')
@@ -63,14 +63,14 @@ test('[composeFilters] should return false if the first of 2 filters return fals
   td.when(f1()).thenReturn(false)
   td.when(f2()).thenReturn(true)
 
-  const subject = composeFilters(f1, f2);
+  const subject = composeFilters(f1, f2)
   const result = subject()
 
   t.notOk(result,
-    'composed filter should return false if the first of 2 filters return false');
+    'composed filter should return false if the first of 2 filters return false')
 
-  t.end();
-});
+  t.end()
+})
 
 test('[composeFilters] should return false if the second of 2 filters return false', t => {
   const f1 = td.func('one')
@@ -78,11 +78,11 @@ test('[composeFilters] should return false if the second of 2 filters return fal
   td.when(f1()).thenReturn(true)
   td.when(f2()).thenReturn(false)
 
-  const subject = composeFilters(f1, f2);
+  const subject = composeFilters(f1, f2)
   const result = subject()
 
   t.notOk(result,
-    'composed filter should return false if the second of 2 filters return false');
+    'composed filter should return false if the second of 2 filters return false')
 
-  t.end();
-});
+  t.end()
+})

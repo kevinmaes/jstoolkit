19c10a28545ca6146a36f0947a51cc72917b08d6
Kevin Maes <kevin@tek212.com>
2016-10-24 19:04:30 -0400

    refactor to mocha

--- src/utils/__test__/index.spec.js
+++ src/utils/__test__/index.spec.js
@@ -1,27 +1,26 @@
-// import { test } from '../../spec_helper'
-// import { toKeys } from '../'
-//
-// test('[toKeys]', t => {
-//   const array = ['a', 'b', 'c']
-//
-//   const result = toKeys(array)
-//
-//   t.deepEquals(result, array, 'keys should be a similar array')
-//
-//   t.end()
-// });
-//
-// test('[toKeys]', t => {
-//   const obj = {
-//     a: null,
-//     b: null,
-//     c: null
-//   };
-//
-//   const result = toKeys(obj)
-//
-//   t.deepEquals(result, ['a', 'b', 'c'],
-//     'keys should be an array of the object keys')
-//
-//   t.end()
-// })
+import { expect } from '../../spec_helper'
+import { toKeys } from '../'
+
+describe('#toKeys()', () => {
+
+  it('should return an array with the same string elements', () => {
+    const array = ['a', 'b', 'c']
+
+    const result = toKeys(array)
+
+    expect(result).to.eql(array)
+  });
+
+  it('should return an array of the object keys', () => {
+    const obj = {
+      a: null,
+      b: null,
+      c: null
+    };
+
+    const result = toKeys(obj)
+
+    expect(result).to.eql(['a', 'b', 'c'])
+  })
+
+})

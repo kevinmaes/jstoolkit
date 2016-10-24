e24151d103f94b442eca1732d7f71d9328ac4eee
Kevin Maes <kevin@tek212.com>
2016-10-24 18:58:08 -0400

    refactor tests to mocha

--- src/__test__/filterMap.spec.js
+++ src/__test__/filterMap.spec.js
@@ -1,4 +1,4 @@
-import { expect } from '../spec_helper'
+import { expect, _ } from '../spec_helper'
 import filterMap from '../filterMap'
 
 describe('#filterMap()', () => {
@@ -20,135 +20,120 @@ describe('#filterMap()', () => {
     expect(result).to.eql([2, 4])
   });
 
-it('should return an empty array when passing in a filter function, "isString"', () => {
-  const array = [1, 2, 3, 4, 5]
-  const isString = arg => typeof arg === 'string'
+  it('should return an empty array when passing in a filter function, "isString"', () => {
+    const array = [1, 2, 3, 4, 5]
+    const isString = arg => typeof arg === 'string'
 
-  const result = filterMap(isString, undefined, array);
+    const result = filterMap(isString, undefined, array);
 
-  expect(result).to.eql([])
-});
+    expect(result).to.eql([])
+  });
 
-it('should return an array of object values when passing in a map function', () => {
-  const array = [
-    { id: 1, val: '1' },
-    { id: 2, val: '2' },
-    { id: 3, val: '3' },
-    { id: 4, val: '4' },
-    { id: 5, val: '5' }
-  ]
-  const getVal = arg => arg.val
+  it('should return an array of object values when passing in a map function', () => {
+    const array = [
+      { id: 1, val: '1' },
+      { id: 2, val: '2' },
+      { id: 3, val: '3' },
+      { id: 4, val: '4' },
+      { id: 5, val: '5' }
+    ]
+    const getVal = arg => arg.val
 
-  const result = filterMap(undefined, getVal, array);
+    const result = filterMap(undefined, getVal, array);
 
-  expect(result).to.eql(['1', '2', '3', '4', '5'])
-});
+    expect(result).to.eql(['1', '2', '3', '4', '5'])
+  });
 
-it('should return an array of values when passing in filter and map functions', () => {
-  const array = [
-    { id: 1, val: '1' },
-    { id: 2, val: '2' },
-    { id: 3, val: '3' },
-    { id: 4, val: '4' },
-    { id: 5, val: '5' }
-  ]
-  const isEven = arg => !(arg.id % 2)
-  const getVal = arg => arg.val
+  it('should return an array of values when passing in filter and map functions', () => {
+    const array = [
+      { id: 1, val: '1' },
+      { id: 2, val: '2' },
+      { id: 3, val: '3' },
+      { id: 4, val: '4' },
+      { id: 5, val: '5' }
+    ]
+    const isEven = arg => !(arg.id % 2)
+    const getVal = arg => arg.val
 
-  const result = filterMap(isEven, getVal, array);
+    const result = filterMap(isEven, getVal, array);
 
-  expect(result).to.eql(['2', '4'])
-});
-//
-// it('should passing in a composed filter function and map function', () => {
-//   const array = [
-//     { id: 1, val: '1' },
-//     { id: 2, val: '2' },
-//     { id: 3, val: '3' },
-//     { id: 4, val: '4' },
-//     { id: 5, val: '5' }
-//   ]
-//   const id = arg => arg.id
-//   const isEven = arg => !(arg % 2)
-//   const idIsEven = _.compose(isEven, id)
-//   const getVal = arg => arg.val
-//
-//   const result = filterMap(idIsEven, getVal, array);
-//
-//   t.deepEquals(result, ['2', '4'],
-//     'return value should be an array of values of the composite filtered object array');
-//
-//   t.end();
-// });
-//
-// it('should passing in comosed filter and map functions', () => {
-//   const array = [
-//     { id: 1, val: '1' },
-//     { id: 2, val: '2' },
-//     { id: 3, val: '3' },
-//     { id: 4, val: '4' },
-//     { id: 5, val: '5' }
-//   ]
-//   const id = arg => arg.id
-//   const isEven = arg => !(arg % 2)
-//   const idIsEven = _.compose(isEven, id)
-//   const getVal = arg => arg.val
-//   const getValAsNumber = _.compose(Number, getVal)
-//
-//   const result = filterMap(idIsEven, getValAsNumber, array);
-//
-//   t.deepEquals(result, [2, 4],
-//     'return value should be an array of values of the composite filtered object array');
-//
-//   t.end();
-// });
-//
-// it('should passing in an optional "until" value', () => {
-//   const array = [1, 2, 3, 4, 5]
-//   const isOdd = arg => !!(arg % 2)
-//
-//   const result = filterMap(isOdd, undefined, array, 2);
-//
-//   t.deepEquals(result, [1, 3],
-//     'return value should be an array containing only the first 2 odd values');
-//
-//   t.end();
-// });
-//
-// it('should passing in filter function and a map property string', () => {
-//   const array = [
-//     { id: 1, val: '1' },
-//     { id: 2, val: '2' },
-//     { id: 3, val: '3' },
-//     { id: 4, val: '4' },
-//     { id: 5, val: '5' }
-//   ]
-//   const isEven = arg => !(arg.id % 2)
-//
-//   const result = filterMap(isEven, 'val', array);
-//
-//   t.deepEquals(result, ['2', '4'],
-//     'return value should be an array of values of the filtered object array');
-//
-//   t.end();
-// });
-//
-// it('should passing in filter function and a map property string (method name)', () => {
-//   const array = [
-//     { id: 1, val: () => '1' },
-//     { id: 2, val: () => '2' },
-//     { id: 3, val: () => '3' },
-//     { id: 4, val: () => '4' },
-//     { id: 5, val: () => '5' }
-//   ]
-//   const isEven = arg => !(arg.id % 2)
-//
-//   const result = filterMap(isEven, 'val', array);
-//
-//   t.deepEquals(result, ['2', '4'],
-//     'return value should be an array of result values of the filtered object array');
-//
-//   t.end();
-// });
+    expect(result).to.eql(['2', '4'])
+  });
+
+  it('should return an array of values when passing in a composed filter function and a map funtion.', () => {
+    const array = [
+      { id: 1, val: '1' },
+      { id: 2, val: '2' },
+      { id: 3, val: '3' },
+      { id: 4, val: '4' },
+      { id: 5, val: '5' }
+    ]
+    const id = arg => arg.id
+    const isEven = arg => !(arg % 2)
+    const idIsEven = _.compose(isEven, id)
+    const getVal = arg => arg.val
+
+    const result = filterMap(idIsEven, getVal, array);
+
+    expect(result).to.eql(['2', '4'])
+  });
+
+  it('should return an array of values when passing in composed filter and map functions', () => {
+    const array = [
+      { id: 1, val: '1' },
+      { id: 2, val: '2' },
+      { id: 3, val: '3' },
+      { id: 4, val: '4' },
+      { id: 5, val: '5' }
+    ]
+    const id = arg => arg.id
+    const isEven = arg => !(arg % 2)
+    const idIsEven = _.compose(isEven, id)
+    const getVal = arg => arg.val
+    const getValAsNumber = _.compose(Number, getVal)
+
+    const result = filterMap(idIsEven, getValAsNumber, array);
+
+    expect(result).to.eql([2, 4])
+  });
+
+  it('should return an array with only the first 2 odd values when passing in an optional "until" arg.', () => {
+    const array = [1, 2, 3, 4, 5]
+    const isOdd = arg => !!(arg % 2)
+
+    const result = filterMap(isOdd, undefined, array, 2);
+
+    expect(result).to.eql([1, 3])
+  });
+
+  it('should return an array of values when passing in a filter function and a map property string', () => {
+    const array = [
+      { id: 1, val: '1' },
+      { id: 2, val: '2' },
+      { id: 3, val: '3' },
+      { id: 4, val: '4' },
+      { id: 5, val: '5' }
+    ]
+    const isEven = arg => !(arg.id % 2)
+
+    const result = filterMap(isEven, 'val', array);
+
+    expect(result).to.eql(['2', '4'])
+  });
+
+  it('should return an array of result values when passing a filter function and a map property string (method name)', () => {
+    const array = [
+      { id: 1, val: () => '1' },
+      { id: 2, val: () => '2' },
+      { id: 3, val: () => '3' },
+      { id: 4, val: () => '4' },
+      { id: 5, val: () => '5' }
+    ]
+    const isEven = arg => !(arg.id % 2)
+
+    const result = filterMap(isEven, 'val', array);
+
+    expect(result).to.eql(['2', '4'])
+  });
 
 })
